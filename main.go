e01f1e39f1b43aa415b4c67f99a10febad34febd
Luminous <1094038421@qq.com>
2025-07-29 16:14:59 +0800

    集成 Gin

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	r := gin.New()
+
+	r.Use(gin.Logger(), gin.Recovery())
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello world",
+		})
+	})
+
+	r.NoRoute(func(c *gin.Context) {
+		acceptString := c.Request.Header.Get("Accept")
+
+		if strings.Contains(acceptString, "text/html") {
+			c.String(http.StatusNotFound, "404 page not found")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    http.StatusNotFound,
+				"error_message": "404 page not found",
+			})
+		}
+	})
+
+	r.Run(":8080")
 }
